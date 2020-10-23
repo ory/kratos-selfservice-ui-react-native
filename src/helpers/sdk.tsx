4c2f766466c5e40f08ab2d1702bf2ea2e6a73c8d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-10-23 14:38:17 +0200

    fix: force credentials false

--- src/helpers/sdk.tsx
+++ src/helpers/sdk.tsx
@@ -1,14 +1,5 @@
-import {
-  Configuration,
-  LoginViaApiResponse,
-  PublicApi,
-  RegistrationViaApiResponse,
-  Session
-} from '@oryd/kratos-client'
+import { Configuration, PublicApi } from '@oryd/kratos-client'
 import Constants from 'expo-constants'
-import { CompleteSelfServiceLoginFlowWithPasswordMethod } from '@oryd/kratos-client/api'
-import { SessionContext, setAuthenticatedSession } from './auth'
-import { handleFormSubmitError } from './form'
 
 // canonicalize removes the trailing slash from URLs.
 const canonicalize = (url: string = '') => url.replace(/\/+$/, '')
@@ -16,10 +7,30 @@ const canonicalize = (url: string = '') => url.replace(/\/+$/, '')
 export const kratosUrl =
   canonicalize(Constants.manifest?.extra?.kratosUrl) || ''
 
-const kratos = new PublicApi(new Configuration({ basePath: kratosUrl }))
+// axios.defaults.withCredentials = false
+const kratos = new PublicApi(
+  new Configuration({
+    basePath: kratosUrl,
+    baseOptions: {
+      // Setting this is very important as axios will send the CSRF cookie otherwise
+      // which causes problems with ORY Kratos' security detection.
+      withCredentials: false
+    }
+  })
+)
 
 export const kratosWithSessionToken = (token: string) =>
-  new PublicApi(new Configuration({ basePath: kratosUrl, apiKey: token }))
+  new PublicApi(
+    new Configuration({
+      basePath: kratosUrl,
+      apiKey: token,
+      baseOptions: {
+        // Setting this is very important as axios will send the CSRF cookie otherwise
+        // which causes problems with ORY Kratos' security detection.
+        withCredentials: false
+      }
+    })
+  )
 
 // This exports the ORY Kratos SDK
 export default kratos
