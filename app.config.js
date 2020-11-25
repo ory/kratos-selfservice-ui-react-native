beb3ac286c9f4e2a5448f1f317579b94b7f3e461
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-25 12:02:18 +0100

    docs: clarify config values

--- app.config.js
+++ app.config.js
@@ -1,10 +1,15 @@
 export default (parent = {}) => {
+  // We gracefully destruct these parameters to avoid "undefined" errors:
   const { config = {} } = parent
-
   const { env = {} } = process || {}
 
   const {
+    // This is the URL of your deployment. In our case we use the ORY Demo
+    // environment
     KRATOS_URL = 'https://demo.tenants.staging.oryapis.dev/api/kratos/public',
+
+    // We use sentry.io for error tracing. This helps us identify errors
+    // in the distributed packages. You can remove this.
     SENTRY_DSN = 'https://8be94c41dbe34ce1b244935c68165eab@o481709.ingest.sentry.io/5530799'
   } = env
 
